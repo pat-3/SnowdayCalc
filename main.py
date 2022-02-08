a873d1275d80847df071a9f98f8d8e794e486afb
pat <73788322+p-at69@users.noreply.github.com>
2022-02-08 08:33:14 -0500

    Update main.py
    made code partially work

--- main.py
+++ main.py
@@ -2,27 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-def temp_finder():
-    print("Current Weather Search:")
-    city = input("Please enter the city you would like to search: ")
-    state = input("Please enter the State that your city is in: ")
 
-    url = f"https://www.google.com/search?q=weather+{city}+{state}"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, "html.parser")
-    temp = soup.find_all('div', attrs={"class": "BNeawe iBp4i AP7Wnd"})
-    print("################################################")
-    print(f"Tempature outside in {city}, {state} currently is: " + temp[0].text + ".")
-    print("################################################")
-temp_finder()
+print("Current Weather Search:")
+city = input("Please enter the city you would like to search: ")
+state = input("Please enter the State that your city is in: ")
 
-def temp_eval():
-   temperature = temp[0].text
-    
-    if temperature <= 32:
-        print("It is very cold. It is " + temp[0].text + ". There is a chance school could be closed. Keep an eye on the news.")
-    elif temperature >= 65:
-        print("It is going to be warm today. It is currently, "  + temp[0].text + "degrees. Make sure to drink water.")
-    else:
-        print("It will be a normal day. It is currently, "  + temp[0].text + "degrees. Prepare for school as normal.")
-temp_eval()
+url = f"https://www.google.com/search?q=weather+{city}+{state}"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+temp = soup.find_all('div', attrs={"class": "BNeawe iBp4i AP7Wnd"})
+print("################################################")
+print(f"Tempature outside in {city}, {state} currently is: " + temp[0].text + ".")
+print("################################################")
+temperature = temp[0].text
+
+
+if temperature <= 32:
+    print("It is very cold. It is currently " + temp[0].text + ". There is a chance school could be closed. Keep an eye on the news.")
+elif temperature >= 65:
+    print("It is going to be warm today. It is currently "  + temp[0].text + ". Make sure to drink water.")
+else:
+    print("It will be a normal day. It is currently "  + temp[0].text + ". Prepare for school as normal.")
